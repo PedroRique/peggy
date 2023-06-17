7ad03ed97c0eb0615a0bfd6ad4b1f1a4a4dc5146
Pedro Rique <prique@patientnow.com>
2023-06-17 14:38:50 -0300

    feat: add icons + first home title

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { BiRightArrowAlt, BiMap } from "react-icons/bi";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.titleContainer}>
+      <View style={styles.titleView}>
+        <BiMap size={44} color="#00C2FF" />
+        <Text style={styles.title}>Por perto</Text>
+      </View>
+
+      <BiRightArrowAlt size={50} color="#FF9900" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  titleView: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginLeft: 8,
+  },
+});
