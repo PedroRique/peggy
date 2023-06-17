5d0487efcb2e4ac50cfb12d11c7710158ecb596a
Pedro Rique <prique@patientnow.com>
2023-06-17 12:55:03 -0300

    feat: first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+
+export interface ButtonProps {
+  onPress: () => void;
+  title: string;
+}
+
+export default function Button(props: ButtonProps) {
+  const { onPress, title = "Save" } = props;
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+});
