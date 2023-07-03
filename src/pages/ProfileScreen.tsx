a16028637699f86fcd0870821c3516cabf31951c
Pedro Rique <prique@patientnow.com>
2023-07-03 09:33:58 -0300

    feat: add item in profile

--- src/pages/ProfileScreen.tsx
+++ src/pages/ProfileScreen.tsx
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { Avatar } from "../components/Avatar";
 import { Header } from "../components/Header";
 import { Rating } from "../components/Rating";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { BoldText } from "../components/Text/BoldText";
+import { Feather } from "@expo/vector-icons";
 
 export default function ProfileScreen() {
   return (
@@ -24,6 +25,15 @@ export default function ProfileScreen() {
         <BoldText style={styles.peggiesText}>
           <Text style={styles.peggiesTextBig}>120</Text> Peggies
         </BoldText>
+      </View>
+
+      <View style={styles.myItemsContainer}>
+        <View style={styles.myItemsHeader}>
+          <BoldText style={styles.myItemsHeaderTitle}>Seus itens</BoldText>
+          <Pressable style={styles.addItemButton} onPress={() => {}}>
+            <Feather name="plus" color={"#fff"} size={32} />
+          </Pressable>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -47,7 +57,7 @@ const styles = StyleSheet.create({
   avatarBio: {
     color: "#777777",
     fontSize: 16,
-    width: '70%',
+    width: "70%",
   },
   peggiesContainer: {
     display: "flex",
@@ -58,6 +68,7 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
     paddingHorizontal: 12,
     borderRadius: 7,
+    marginBottom: 32,
   },
   peggiesText: {
     fontSize: 24,
@@ -67,5 +78,21 @@ const styles = StyleSheet.create({
   peggiesTextBig: {
     fontSize: 32,
     lineHeight: 24,
+  },
+  myItemsContainer: {},
+  myItemsHeader: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  myItemsHeaderTitle: {
+    fontSize: 24,
+  },
+  addItemButton: {
+    backgroundColor: "#00C2FF",
+    borderRadius: 5,
+    padding: 4,
+    paddingHorizontal: 5,
   },
 });
