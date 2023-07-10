fca9389336380f991b9ebfc4ddcf6f9835d212c2
Pedro Rique <prique@patientnow.com>
2023-07-10 09:53:46 -0300

    feat: adicionando tabs

--- src/pages/LoansScreen.tsx
+++ src/pages/LoansScreen.tsx
@@ -1,12 +1,34 @@
-import { ScrollView, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Header } from "../components/Header";
+
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { Text } from "../components/Text/Text";
+
+const Tab = createMaterialTopTabNavigator();
+
+const LendingTab = () => {
+  return <Text>Emprestando</Text>;
+};
+
+const BorrowingTab = () => {
+  return <Text>Pedindo emprestado</Text>;
+};
+
+function LoanTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Emprestando" component={LendingTab} />
+      <Tab.Screen name="Pegando" component={BorrowingTab} />
+    </Tab.Navigator>
+  );
+}
 
 export default function LoansScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <Header title={"Empréstimos"} />
-      <ScrollView style={styles.scrollContainer}></ScrollView>
+      <LoanTabs></LoanTabs>
     </SafeAreaView>
   );
 }
