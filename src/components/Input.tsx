b3bde06d483e23ca3ff114509283d850103bc037
Pedro Rique <prique@patientnow.com>
2023-07-11 19:08:21 -0300

    feat:xomeço de ajustes de bugs

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,34 +1,56 @@
-import {
-  TextInput as ReactTextInput,
-  TextInputProps,
-  StyleSheet,
-  View,
-} from "react-native";
-import { Text } from "./Text/Text";
-import { BoldText } from "./Text/BoldText";
+import React, { useState } from 'react';
+import { TextInput as ReactTextInput, TextInputProps, StyleSheet, View, TouchableOpacity } from 'react-native';
+import { Text } from './Text/Text';
+import { BoldText } from './Text/BoldText';
+import { Feather } from '@expo/vector-icons';
 
 export const TextInput = ({
   style,
   value,
   multiline,
   label,
+  showEyeIcon = false,
   ...rest
-}: React.PropsWithChildren<TextInputProps & { label?: string }>) => (
-  <View style={{ flex: 1 }}>
-    <BoldText style={styles.label}>{label}</BoldText>
-    <ReactTextInput
-      value={value}
-      multiline={multiline}
-      style={[styles.input, style]}
-      {...rest}
-    ></ReactTextInput>
-    {multiline && <Text style={styles.textLength}>{value?.length}/100</Text>}
-  </View>
-);
+}: React.PropsWithChildren<TextInputProps & { label?: string; showEyeIcon?: boolean }>) => {
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
+
+  const toggleSecureTextEntry = () => {
+    setSecureTextEntry((prevValue) => !prevValue);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <BoldText style={styles.label}>{label}</BoldText>
+      <View style={styles.inputContainer}>
+        <ReactTextInput
+          value={value}
+          multiline={multiline}
+          secureTextEntry={showEyeIcon ? secureTextEntry : false}
+          style={[styles.input, showEyeIcon && styles.inputWithIcon, style]}
+          {...rest}
+        />
+        {showEyeIcon && (
+          <TouchableOpacity
+            style={styles.iconContainer}
+            onPress={toggleSecureTextEntry}
+          >
+            <Feather
+              name={secureTextEntry ? 'eye-off' : 'eye'}
+              size={24}
+              color="gray"
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+      {multiline && <Text style={styles.textLength}>{value?.length}/100</Text>}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   input: {
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     padding: 16,
     borderRadius: 8,
     fontSize: 18,
@@ -38,13 +60,24 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
     elevation: 10,
   },
+  inputWithIcon: {
+    paddingRight: 36,
+  },
   textLength: {
     fontSize: 12,
-    position: "absolute",
+    position: 'absolute',
     bottom: 0,
     right: 0,
   },
   label: {
     marginBottom: 4,
+  },
+  iconContainer: {
+    position: 'absolute',
+    top: 16,
+    right: 16,
+  },
+  icon: {
+    opacity: 0.6,
   },
 });
--- src/pages/RegisterScreen.tsx
+++ src/pages/RegisterScreen.tsx
@@ -5,10 +5,8 @@ import { BoldText } from "../components/Text/BoldText";
 import { userSlice } from "../store/slices/user.slice";
 import Button from "../components/Button";
 import { createUser} from "../services/user.service";
-import { Feather } from "@expo/vector-icons";
 import { useDispatch } from "react-redux";
 import { TextInput } from "../components/Input";
-
 import { useNavigation } from "@react-navigation/native";
 import { StackTypes } from "../../App";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -17,38 +15,42 @@ const Login = require("../../assets/images/Logo/peggy-logo.png");
 export default function RegisterScreen() {
   const dispatch = useDispatch();
   const navigation = useNavigation<StackTypes>()
-  const [secureTextEntry, setSecureTextEntry] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
 
-  const toggleSecureEntry = () => {
-    setSecureTextEntry(!secureTextEntry);
-  };
 
   const registerUser = async () => {
-    if (name === "" || email === "" || password === "") {
-      console.log("Preencha nome, email e senha");
+    if (name === '' || email === '' || password === '') {
+      console.log('Preencha nome, email e senha');
       return;
     }
-  
+
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     if (!emailRegex.test(email)) {
-      console.log("Por favor, insira um e-mail válido");
+      console.log('Por favor, insira um e-mail válido');
       return;
     }
-  
+
     const result = await createUser({
       name,
       email,
       password,
     });
-  
+
     if (result !== null && result !== undefined) {
-      console.log("Cadastro bem-sucedido");
-      navigation.navigate("Main")
+      console.log('Cadastro bem-sucedido');
+      dispatch(userSlice.actions.setProfile({
+        name: displayName,
+        email,
+        uid: "",
+        photoURL: null,
+        addresses: [],
+        rate: 0
+      }));
+      navigation.navigate('Main');
     } else {
-      console.log("Credenciais inválidas");
+      console.log('Credenciais inválidas');
     }
   };
   
@@ -61,8 +63,8 @@ export default function RegisterScreen() {
       <View style={styles.loginContainer}>
         <View>
           <View>
-            <BoldText style={styles.title}>Nome</BoldText>
             <TextInput
+              label="Nome"
               style={styles.input}
               placeholder="Insira seu nome"
               value={name}
@@ -70,9 +72,9 @@ export default function RegisterScreen() {
             />
           </View>
           <View>
-            <BoldText style={styles.title}>E-mail</BoldText>
             <View style={styles.passwordContainer}>
               <TextInput
+                label="E-mail"
                 style={styles.input}
                 placeholder="Insira seu e-mail"
                 value={email}
@@ -80,11 +82,11 @@ export default function RegisterScreen() {
               />
             </View>
             <View>
-              <BoldText style={styles.title}>Senha</BoldText>
               <TextInput
+                label="Senha"
                 style={styles.input}
                 placeholder="Insira sua senha"
-                secureTextEntry={secureTextEntry}
+                showEyeIcon={true}
                 value={password}
                 onChangeText={setPassword}
               />
@@ -140,7 +142,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 10,
     elevation: 10,
-    marginBottom:16,
+    
   },
   passwordContainer: {
     position: "relative",
