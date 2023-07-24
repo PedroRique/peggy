399b5ef733ee9cf0ff542b2084a602775e296b4f
Pedro Rique <prique@patientnow.com>
2023-07-24 20:07:54 -0300

    feat: ao atualizar o status do loan, atualizar o locked to produto

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import functions = require("firebase-functions");
 import admin = require("firebase-admin");
 import sendPushNotification from "./sendPushNotification";
+import updateProductLockedStatus from "./updateProductLockedStatus";
 
 admin.initializeApp();
 
@@ -37,4 +38,27 @@ exports.sendLoanRequestNotification = functions.firestore
       pushToken: data.pushToken,
       message: "Nova solicitação de empréstimo!",
     });
+  });
+
+const loanStatusesToUpdateLocked = [2, 4];
+const loanStatusesToUpdateUnlocked = [1, 3, 5, 6];
+
+exports.updateProductLockedStatus = functions.firestore
+  .document("loans/{loanId}")
+  .onUpdate((change) => {
+    const newValue = change.after.data();
+    const previousValue = change.before.data();
+
+    if (newValue.status !== previousValue.status) {
+      const productId = newValue.productId;
+      const loanStatus = newValue.status;
+
+      if (loanStatusesToUpdateLocked.includes(loanStatus)) {
+        return updateProductLockedStatus(productId, true);
+      } else if (loanStatusesToUpdateUnlocked.includes(loanStatus)) {
+        return updateProductLockedStatus(productId, false);
+      }
+    }
+
+    return null;
   });
