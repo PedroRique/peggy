5557340f689faebd7bc5eb6812b7076e8387aa59
Pedro Rique <prique@patientnow.com>
2023-07-23 22:20:35 -0300

    feat: remove unused function

--- App.tsx
+++ App.tsx
@@ -46,27 +46,6 @@ Notifications.setNotificationHandler({
   }),
 });
 
-// Can use this function below OR use Expo's Push Notification Tool from: https://expo.dev/notifications
-async function sendPushNotification(expoPushToken: any) {
-  const message = {
-    to: expoPushToken,
-    sound: "default",
-    title: "Original Title",
-    body: "And here is the body!",
-    data: { someData: "goes here" },
-  };
-
-  await fetch("https://exp.host/--/api/v2/push/send", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Accept-encoding": "gzip, deflate",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(message),
-  });
-}
-
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
