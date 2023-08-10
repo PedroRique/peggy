dd74ee2b9e37543c7150f4294d3e8ed017956b66
ThiagoFTeixeira <thiago.teixeira2004@gmail.com>
2023-08-10 01:44:55 -0300

    feat: adicionando dropdown com endereços que possuo

--- src/pages/NearbyScreen.tsx
+++ src/pages/NearbyScreen.tsx
@@ -9,11 +9,20 @@ import { BoldText } from "../components/Text/BoldText";
 import { AppState } from "../store";
 import { loanSlice } from "../store/slices/loan.slice";
 import { productSlice } from "../store/slices/product.slice";
+import React, { useState } from "react";
+import DropDown from "react-native-paper-dropdown";
+import { formatAddressLabel } from "../services/utils.service";
+import { Address } from "../models/Address";
 
 export const NearbyScreen = () => {
   const dispatch = useDispatch();
   const navigation = useNavigation<StackTypes>();
   const products = useSelector((state: AppState) => state.product.nearProducts);
+
+  const [showMyAddressesDropDown, setShowMyAddressesDropDown] = useState(false);
+  const [selectedAddress, setSelectedAddress] = useState("");
+
+  const currentUserData = useSelector((state: AppState) => state.user.userData);
 
   return (
     <View>
@@ -22,7 +31,25 @@ export const NearbyScreen = () => {
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <BoldText size={24}>Onde você está?</BoldText>
 
-        <TextInput placeholder="Selecione um endereço" />
+        <DropDown
+          label={"Selecione um endereço"}
+          mode={"outlined"}
+          visible={showMyAddressesDropDown}
+          showDropDown={() => setShowMyAddressesDropDown(true)}
+          onDismiss={() => setShowMyAddressesDropDown(false)}
+          value={selectedAddress}
+          setValue={(addressLabel) => {
+            setSelectedAddress(addressLabel);
+          }}
+          list={
+            currentUserData && currentUserData.addresses
+              ? currentUserData.addresses.map((address: Address) => ({
+                  label: formatAddressLabel(address),
+                  value: formatAddressLabel(address),
+                }))
+              : []
+          }
+        />
 
         <View style={styles.products}>
           {products.map((product, i) => (
