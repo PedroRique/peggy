12312903d7a22f510eb059dcc9757c69bf90c857
Thiago Teixeira <thiago.teixeira2004@gmail.com>
2023-12-04 13:02:02 -0300

    feat:resolvendo problema do LoansScreen crashar

--- src/pages/LoansScreen.tsx
+++ src/pages/LoansScreen.tsx
@@ -37,7 +37,7 @@ const LoansTab = ({ type }: { type: LoanType }) => {
   const getLoans = async () => {
     try {
       const result = await fetchLoansWithProductInfo(type);
-
+      console.log(result);
       checkForPendingRates(result);
       const groupedLoans = groupLoansBySection(result);
 
